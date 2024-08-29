8e796368344a3e5dec5302f0645cbac32337a407
Steve Benson <steve@xelphene.net>
2024-08-29 05:02:20 +0000

    add isNode to index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 exports.NODE = require('./consts').NODE;
 exports.nodeOf = require('./node/util').nodeOf;
 exports.hasNode = require('./node/util').hasNode;
+exports.isNode = require('./node/util').isNode;
 
 exports.ComputeNode = require('./node/compute').ComputeNode;
 exports.InputNode = require('./node/input').InputNode;
