a919fe9006cfe5f7d36527f704c7ac56557f0cd0
Steve Benson <steve@xelphene.net>
2024-09-02 00:36:42 +0000

    maxNode is now optional for StrechNode and can be assigned later.

--- node/stretch.js
+++ node/stretch.js
@@ -10,12 +10,15 @@ const {getNodeValueProxy} = require('./nvp');
 class StretchNode extends Node {
     constructor({maxNode, debugName}) {
         super({debugName});
-        //if( ! (maxNode instanceof Node) )
-        if( ! isNode(maxNode) )
-            throw new Error(`Node instance required for maxNode argument`);
-        maxNode = nodeOf(maxNode);
-        this._maxNode = maxNode;
-        this.listenTo(maxNode);
+
+        if( maxNode!==undefined ) {
+            if( ! isNode(maxNode) )
+                throw new Error(`Node instance or undefined required for maxNode argument`);
+            maxNode = nodeOf(maxNode);
+            this._maxNode = maxNode;
+            this.listenTo(maxNode);
+        } else
+            this._maxNode = null;
         
         this._value = null;
         this._computeCount = 0;
@@ -24,7 +27,18 @@ class StretchNode extends Node {
     get settable () { return true }
     get fresh () { return true }
     get computeCount () { return this._computeCount }
-        
+
+    get maxNode () { return this._maxNode }
+    set maxNode (maxNode) {
+        if( this._maxNode !== null )
+            throw new Error(`maxNode already set`);
+        if( ! isNode(maxNode) )
+            throw new Error(`Node instance required for maxNode argument`);
+        maxNode = nodeOf(maxNode);
+        this._maxNode = maxNode;
+        this.listenTo(maxNode);
+    }
+
     // called exclusively from Channel when our maxNode changed
     hearSpoiled(speakingNode) {
         this.saySpoiled();
@@ -39,7 +53,7 @@ class StretchNode extends Node {
         if( typeof(v) !== 'number' )
             throw new TypeError('number type required for StrechNode assigned value');
         
-        if( v > this._maxNode.rawValue )
+        if( this._maxNode!==null && v > this._maxNode.rawValue )
             throw new Error(`Out of bounds value ${v}: max is ${this._maxNode.rawValue}`);
         this._value = new NodeValue(this, v);
         this.sayChanged();
@@ -50,7 +64,7 @@ class StretchNode extends Node {
         if( this._value===null )
             throw new Error(`Cannot compute the value of a StrechNode before it has been assigned a value.`);
         
-        if( this._value <= this._maxNode.rawValue )
+        if( this._maxNode!==null && this._value <= this._maxNode.rawValue )
             return this._value;
         else
             return this._maxNode.rawValue;
