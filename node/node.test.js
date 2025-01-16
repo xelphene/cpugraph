ed6fed47ce52d22afd5efb78e9a877d9e3ef187d
Steve Benson <steve@xelphene.net>
2025-01-16 11:25:44 +0000

    Change BlabListenerMixin.hearingFromNodes to hearingFromObjects

--- node/node.test.js
+++ node/node.test.js
@@ -39,9 +39,9 @@ test('depvary', () =>
     expect( i1.debugName ).toBe( 'i1' );
     expect( t.i2.debugName ).toBe( 'i2' );
     expect( 0 + t.c.value ).toBe( 2 );
-    expect( t.c.hearingFromNodes ).toContain( i1 );
-    expect( t.c.hearingFromNodes ).toContain( t.mode );
-    expect( t.c.hearingFromNodes ).not.toContain( t.i2 );
+    expect( t.c.hearingFromObjects ).toContain( i1 );
+    expect( t.c.hearingFromObjects ).toContain( t.mode );
+    expect( t.c.hearingFromObjects ).not.toContain( t.i2 );
     expect( t.c.fresh ).toBe( true );
     expect( t.c.computeCount ).toBe( 1 );
 
@@ -51,9 +51,9 @@ test('depvary', () =>
     expect( t.c.fresh ).toBe( true );
     expect( 0 + t.c.value ).toBe( 2 );
     expect( t.c.computeCount ).toBe( 1 );
-    expect( t.c.hearingFromNodes ).toContain( i1 );
-    expect( t.c.hearingFromNodes ).toContain( t.mode );
-    expect( t.c.hearingFromNodes ).not.toContain( t.i2 );
+    expect( t.c.hearingFromObjects ).toContain( i1 );
+    expect( t.c.hearingFromObjects ).toContain( t.mode );
+    expect( t.c.hearingFromObjects ).not.toContain( t.i2 );
     
     //console.log(`changing inputs...`);
     t.mode.value = 'add';
@@ -64,9 +64,9 @@ test('depvary', () =>
     expect( t.c.fresh ).toBe( true );
     expect( t.c.computeCount ).toBe( 2 );
 
-    expect( t.c.hearingFromNodes ).toContain( i1 );
-    expect( t.c.hearingFromNodes ).toContain( t.mode );
-    expect( t.c.hearingFromNodes ).toContain( t.i2 );
+    expect( t.c.hearingFromObjects ).toContain( i1 );
+    expect( t.c.hearingFromObjects ).toContain( t.mode );
+    expect( t.c.hearingFromObjects ).toContain( t.i2 );
 });
 
 test('nest', () =>
@@ -91,8 +91,8 @@ test('nest', () =>
     t.i.value = 0.1;
     
     expect( 0+t.c.value ).toBe( 222.1 );
-    expect( t.c.hearingFromNodes ).toContain( t.s.x );
-    expect( t.c.hearingFromNodes ).toContain( t.i );
+    expect( t.c.hearingFromObjects ).toContain( t.s.x );
+    expect( t.c.hearingFromObjects ).toContain( t.i );
 });
 
 test('stretch', () =>
