d0877d2bfcb086ffe5c9d1f295767cdeb6ad9230
Steve Benson <steve@xelphene.net>
2025-02-07 09:25:27 +0000

    add Constraint.pertainsToNode()

--- constraint/base.js
+++ constraint/base.js
@@ -8,5 +8,9 @@ class Constraint {
     check () { throw new Error('Not Implemented') }
     
     get nodes () { return [] }
+    
+    pertainsToNode (n) {
+        return this.nodes.includes(n)
+    }
 }
 exports.Constraint = Constraint;
